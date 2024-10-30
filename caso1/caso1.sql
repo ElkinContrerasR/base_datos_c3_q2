f52a0e26c37d193f97272caf40cc7819bfd58bdd
Elkin Contreras <escontreras-2023a@corhuila.edu.co>
2024-10-30 06:39:15 -0500

    Caso 1

---
+++ caso1/caso1.sql
@@ -0,0 +1,8 @@
+SELECT 
+        * 
+    FROM 
+        role r
+    inner join role_module rm ON r.id = rm.role_id
+    inner join module m ON role_module.module_id = module.id
+    inner join module_view mv ON m.id = mv.module_id
+    inner join view v ON mv.view_id = v.id
